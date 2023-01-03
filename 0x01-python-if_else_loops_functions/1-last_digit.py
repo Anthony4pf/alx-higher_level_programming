5f1eb77971ac58b6079fc79b8d58c203990e8418
Anthony Ede <edeanthony14@gmail.com>
2023-01-03 15:27:39 -0800

    Update Last Digit code to check for negative inytegers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+if number < 0:
+    number -= number
 digit = number % 10
 print(f"Last digit of {number} is {digit}", end=" ")
 if digit > 5:
