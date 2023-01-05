c8e5728c11d64369a36842989e5863c07346d46c
Anthony Ede <edeanthony14@gmail.com>
2023-01-05 11:52:05 -0800

    add 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    argc = len(argv)
+    if argc == 1:
+        print("{}".format(argc - 1))
+    else:
+        sum_arg = 0
+        for i in range(1, argc):
+            sum_arg += int(argv[i])
+        print("{}".format(sum_arg)) 
