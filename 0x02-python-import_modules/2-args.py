f7036ab0ca80ae23706790a10eed431d197aa2f0
Anthony Ede <edeanthony14@gmail.com>
2023-01-05 10:55:28 -0800

    Fix string formats

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     len_argv = len(sys.argv)
 
     if len_argv == 1:
-        print("0 arguments.")
+        print("{} arguments.".format(len_argv - 1))
     else:
         print("{} arguments:".format(len_argv - 1))
         i = 1
