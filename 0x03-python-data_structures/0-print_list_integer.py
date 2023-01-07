1b6c1a9a13ca2e2104b1343806359d58b0f34529
Anthony Ede <edeanthony14@gmail.com>
2023-01-07 08:57:34 -0800

    Update Print Loop in File_0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    length = len(my_list)
-
-    i = 0
-    while i < length:
-        print("{}".format(my_list[i]))
-        i += 1
+    for list in my_list:
+        print("{}".format(list))
