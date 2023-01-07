e175eee0a3c6dcac56824e70fd4966a58a3da86c
Anthony Ede <edeanthony14@gmail.com>
2023-01-07 08:48:09 -0800

    Add New Files

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    length = len(my_list)
+
+    i = 0
+    while i < length:
+        print(my_list[i])
+        i += 1
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,5 @@
+# Data Structures in Python
+
+This module covers the basics of data structures in Python:
+- Lists
+- Tuples
