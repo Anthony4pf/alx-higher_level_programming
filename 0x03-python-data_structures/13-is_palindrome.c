56d404b9d1165ea1ba20182410e790ab13d28bf8
Anthony Ede <edeanthony14@gmail.com>
2023-01-09 01:08:59 -0800

    Update Fixes

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,8 +11,8 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *ptr = NULL, *temp = NULL;
-	int count = 0, i = 0, j = 0, len = 0;
-	int *l_array, *r_array;
+	int count = 0, i = 0, j = 0; //len = 0;
+	int *l_array; //*r_array;
 	
 	ptr = *head;
 
@@ -27,28 +27,28 @@ int is_palindrome(listint_t **head)
 	l_array = malloc(sizeof(int) * count);
 	if (l_array == NULL)
 		return (-1);
-	r_array = malloc(sizeof(int) * count);
+/*	r_array = malloc(sizeof(int) * count);
 	if (r_array == NULL)
-		return (-1);
+		return (-1);*/
 	temp = *head;
-	len = count;
+	//len = count;
 	while (temp != NULL)
 	{
 		l_array[i] = temp->n;
-		r_array[len - i - 1] = temp->n;
+		// r_array[len - i - 1] = temp->n;
 		i++;
 		temp = temp->next;
 	}
-	for (j = 0; j < count; j++)
+	for (j = 0; j < count / 2; j++)
 	{
-		if (l_array[j] != r_array[j])
+		if (l_array[j] != l_array[count - j - 1])
 		{
 			free(l_array);
-			free(r_array);
+			//free(r_array);
 			return (0);
 		}
 	}
 	free(l_array);
-	free(r_array);
+	//free(r_array);
 	return (1);
 }
