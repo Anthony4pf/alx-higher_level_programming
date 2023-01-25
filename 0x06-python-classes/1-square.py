6e92242c6384f0acd74fcf71bd53478633b8ce2c
Anthony Ede <edeanthony14@gmail.com>
2023-01-25 08:59:48 -0800

    Add New File_1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This Class Defines a Square"""
+
+
+class Square:
+    """Represents a Square Class
+    Atrributes:
+        __size (int): size of the side of the square
+    """
+
+    def __int__(self, size):
+        """Initializes a Square
+        Args:
+            size: side of the square
+        Returns: None
+        """
+        self.__size = size
