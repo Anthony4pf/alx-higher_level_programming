19fd6d381a5e9c85235f339ca4eb7f7fc5b9d893
Anthony Ede <edeanthony14@gmail.com>
2023-01-25 09:20:59 -0800

    Add New File_3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Square Class Definition"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate the area of a square
+        Returns: area of the square
+        """
+        return self.__size ** 2
