ec4d6deb6d37d7ef9da55fe549557587b3a3f0e5
Anthony Ede <edeanthony14@gmail.com>
2023-02-02 08:51:55 -0800

    Add testfile of 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -9,8 +9,8 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-
-    for i in range(size):
-        for x in range(size):
-            print("#", end="")
-        print()
+    if size > 0:
+        for i in range(size):
+            for x in range(size):
+                print("#", end="")
+            print()
