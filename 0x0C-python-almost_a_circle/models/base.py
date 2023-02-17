c45e286e705fbec2d2ef3e5a5f08db65cf161aa1
Anthony Ede <edeanthony14@gmail.com>
2023-02-17 15:24:08 -0800

    Fix Errors in base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,8 +20,8 @@ class Base:
         """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
