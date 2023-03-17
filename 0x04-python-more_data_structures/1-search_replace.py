8e5aefd65ae80e76b8c849a2355669fea8f9b735
Anthony Ede <edeanthony14@gmail.com>
2023-03-17 14:40:46 -0700

    Improve Efficiency in File_3

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,6 +4,6 @@ def search_replace(my_list, search, replace):
 
     for i, num in enumerate(new_list):
         if num == search:
-            new_list[i] = replace
+           new_list[i] = replace
 
     return new_list
--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    new_set = []
-    for elem in set(set_1):
-        for index in set(set_2):
-            if elem == index:
-                new_set.append(elem)
-
-    return (new_set)
+    return set_1 & set_2
