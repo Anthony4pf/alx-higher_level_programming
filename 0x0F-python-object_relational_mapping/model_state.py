24c03c2e0a42ff8d289ebcbd1c366fb201f606f7
Anthony Ede <edeanthony14@gmail.com>
2023-06-19 17:21:42 -0700

    Remove import of create_engine

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bi/python3
 """This module contains State class"""
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -9,7 +9,7 @@ Base = declarative_base()
 
 class State(Base):
     """Definition of the State Model"""
-    __tablename__ = "states"
+    __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
